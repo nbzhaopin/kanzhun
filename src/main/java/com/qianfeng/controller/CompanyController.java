92930ff16aa7fd9f3f2f96eec5ce273b5a0cb67b
用户名 <1262967980@qq.com>
2019-01-09 22:33:49 +0800

    shou-genggai

--- src/main/java/com/qianfeng/controller/CompanyController.java
+++ src/main/java/com/qianfeng/controller/CompanyController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qianfeng.pojo.Company;
 import com.qianfeng.service.CompanyService;
 import com.qianfeng.vo.CompanyVo;
 import com.qianfeng.vo.PageBeanVo;
@@ -15,16 +16,16 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @RequestMapping("/com")
 @CrossOrigin
-@Api(produces = "这是Java编写的接口文档",value = "接口文档")
+@Api(produces = "这是Java编写的接口文档",value = "首页-公司")
 public class CompanyController {
 	
 	@Autowired
 	private CompanyService cse;
 	
-	@ApiOperation(notes = "实现传递参数，响应指定数量的数据",tags = {"数量","名次"},value = "获取名次接口")
+	@ApiOperation(notes = "实现传递参数，响应指定数量的数据",value = "看公司-公司信息展示以及条件查询展示")
 	@RequestMapping("/showAll.do")
-	public PageBeanVo<CompanyVo> showAll(int page, int limit, String cname, String pname, String grade, String level) {
+	public PageBeanVo<Company> showAll(int page, int limit, String ciname, String pname, String grade, String level) {
 		
-		return cse.selectByTJ(page, limit, cname, pname, grade, level);
+		return cse.selectByTJ(page, limit, ciname, pname, grade, level);
 	}
 }
